1bbeac4ff43042d2e849564e1f1c6b511578ffd7
Kunal Singh <kunalsin9h@gmail.com>
2023-02-11 00:07:29 +0530

    updated main.ts

--- cmd/bot/main.ts
+++ cmd/bot/main.ts
@@ -3,11 +3,13 @@ dotenv.config();
 
 import { NewDiscordClient } from "./client";
 import { loadEvents } from "./event";
+import registerCommands from "./register-commands";
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
 const client = NewDiscordClient();
 
+registerCommands(client.commands);
 loadEvents(client);
 
 client.login(BOT_TOKEN);
