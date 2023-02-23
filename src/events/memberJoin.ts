0d0c8250918df1ab200526d8f0aae528be6390ac
Kunal Singh <kunalsin9h@gmail.com>
2023-02-23 21:14:23 +0530

    added more stuff when someone joins

--- src/events/memberJoin.ts
+++ src/events/memberJoin.ts
@@ -1,4 +1,4 @@
-import { Events, GuildMember, AttachmentBuilder } from "discord.js";
+import { Events, GuildMember, Role, EmbedBuilder } from "discord.js";
 import { ClientEvent } from "@src/event";
 import { getChannel } from "@src/client";
 
@@ -7,27 +7,46 @@ const memberJoin = new ClientEvent(Events.GuildMemberAdd, false);
 memberJoin.execute = async function (member: GuildMember) {
   const welcomeMessageChannelId = process.env.WELCOME;
 
-  if (welcomeMessageChannelId == undefined) {
+  if (welcomeMessageChannelId === undefined) {
     console.log("[EVENTS/MEMBER-JOIN] missing welcome channel id");
     process.exit(1);
   }
 
   const channel = getChannel(welcomeMessageChannelId);
 
-  if (channel == undefined) {
+  if (channel === undefined) {
     console.log("[EVENTS/MEMBER-JOIN] missing welcome channel id");
     process.exit(1);
   }
 
-  const message = `Hey, <@${member.id}> welcome to **COPTIT!**, pick your <#981589280923209868> and do write suggestion in <#1019673354334109767> if you have any!`;
-  const avatar = new AttachmentBuilder(member.displayAvatarURL(), {
-    name: "avatar.png",
-  });
+  const embedMessageToSendWhenUserJoinServer = new EmbedBuilder()
+    .setColor("Green")
+    .setTitle(`Hey! ${member.displayName}, welcome to **COPTIT**!`)
+    .setURL("https://coptit.web.app")
+    .setThumbnail(member.displayAvatarURL())
+    .setTimestamp()
+    .setDescription(
+      `<@${member.id}> here what you can do **next**!\n\n➜ Pick you <#981589280923209868>\n➜ Introduce you self at <#981574102018035724>\n➜ Write any suggestions in <#1019673354334109767> if you have any`
+    );
+
+  const roleToAdd: Role | undefined = member.guild.roles.cache.find(
+    (value: Role) => {
+      return value.name === "member";
+    }
+  );
+
+  if (roleToAdd === undefined) {
+    console.log(
+      "[EVENTS/MEMBER-JOIN] missing role to add user when join server"
+    );
+    process.exit(1);
+  }
+
+  member.roles.add(roleToAdd);
 
   if (channel.isTextBased()) {
     channel.send({
-      content: message,
-      files: [avatar],
+      embeds: [embedMessageToSendWhenUserJoinServer],
     });
   }
 };
